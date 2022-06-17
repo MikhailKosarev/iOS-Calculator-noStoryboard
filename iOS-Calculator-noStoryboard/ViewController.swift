608d7f1c6dca4bdafbc32740100325a692d0b498
Mikhail Kosarev <michael.kosarev@gmail.com>
2022-06-17 20:08:42 +0200

    Initial Commit

---
+++ iOS-Calculator-noStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Calculator-noStoryboard
+//
+//  Created by Mikhail on 17.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
